bd13c07ae10db86e3e01d2ac492f525e0e6b2b2e
evandrocoan <evandrocoan@hotmail.com>
2017-12-06 16:38:14 -0200

    Moved the first and last tests methods to its corresponding class.

--- tests/clear_cursors_carets_first_selection_unit_tests.py
+++ tests/clear_cursors_carets_first_selection_unit_tests.py
@@ -8,12 +8,73 @@ import sublime_plugin
 
 class ClearCursorsCaretsFirstSelectionUnitTests(sys.modules["ClearCursorsCarets.tests.utilities"].BasicSublimeTextViewTestCase):
 
-    @classmethod
-    def setUp(self):
-        super().setUp()
-        self.view.set_syntax_file( "Packages/Text/Plain text.tmLanguage" )
+    def test_1_selections_at_first_word(self):
+        self.create_test_text(0)
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "single_selection_first" )
 
-    def test_first_selection_with_2_selections_at_last_word(self):
+        region = self.view.sel()[0]
+        self.assertEqual( sublime.Region(0, 4), region )
+
+    def test_2_selections_at_first_word(self):
+        self.create_test_text(0)
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "single_selection_first" )
+
+        region = self.view.sel()[0]
+        self.assertEqual( sublime.Region(0, 4), region )
+
+    def test_3_selections_at_first_word(self):
+        self.create_test_text(0)
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "single_selection_first" )
+
+        region = self.view.sel()[0]
+        self.assertEqual( sublime.Region(0, 4), region )
+
+    def test_5_selections_at_first_word(self):
+        self.create_test_text(0)
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "single_selection_first" )
+
+        region = self.view.sel()[0]
+        self.assertEqual( sublime.Region(0, 4), region )
+
+    def test_6_selections_at_first_word(self):
+        self.create_test_text(0)
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "single_selection_first" )
+
+        region = self.view.sel()[0]
+        self.assertEqual( sublime.Region(0, 4), region )
+
+    def test_6_selections_plus_redundant_expand_at_first_word(self):
+        self.create_test_text(0)
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "find_under_expand" )
+        self.view.window().run_command( "single_selection_first" )
+
+        region = self.view.sel()[0]
+        self.assertEqual( sublime.Region(0, 4), region )
+
+    def test_2_selections_at_last_word(self):
         self.create_test_text(26)
         self.view.window().run_command( "find_under_expand" )
         self.view.window().run_command( "find_under_expand" )
@@ -22,7 +83,7 @@ class ClearCursorsCaretsFirstSelectionUnitTests(sys.modules["ClearCursorsCarets.
         region = self.view.sel()[0]
         self.assertEqual( sublime.Region(26, 30), region )
 
-    def test_first_selection_with_3_selections_at_last_word(self):
+    def test_3_selections_at_last_word(self):
         self.create_test_text(21)
         self.view.window().run_command( "find_under_expand" )
         self.view.window().run_command( "find_under_expand" )
