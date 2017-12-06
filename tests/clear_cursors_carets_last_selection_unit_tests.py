d9805570a326c3c113b20068fd8d86563ec5785a
evandrocoan <evandrocoan@hotmail.com>
2017-12-06 16:38:14 -0200

    Moved the first and last tests methods to its corresponding class.

--- tests/clear_cursors_carets_last_selection_unit_tests.py
+++ tests/clear_cursors_carets_last_selection_unit_tests.py
@@ -8,78 +8,7 @@ import sublime_plugin
 
 class ClearCursorsCaretsLastSelectionUnitTests(sys.modules["ClearCursorsCarets.tests.utilities"].BasicSublimeTextViewTestCase):
 
-    @classmethod
-    def setUp(self):
-        super().setUp()
-        self.view.set_syntax_file( "Packages/Text/Plain text.tmLanguage" )
-
-    def test_first_selection_with_1_selections_at_first_word(self):
-        self.create_test_text(0)
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "single_selection_first" )
-
-        region = self.view.sel()[0]
-        self.assertEqual( sublime.Region(0, 4), region )
-
-    def test_first_selection_with_2_selections_at_first_word(self):
-        self.create_test_text(0)
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "single_selection_first" )
-
-        region = self.view.sel()[0]
-        self.assertEqual( sublime.Region(0, 4), region )
-
-    def test_first_selection_with_3_selections_at_first_word(self):
-        self.create_test_text(0)
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "single_selection_first" )
-
-        region = self.view.sel()[0]
-        self.assertEqual( sublime.Region(0, 4), region )
-
-    def test_first_selection_with_5_selections_at_first_word(self):
-        self.create_test_text(0)
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "single_selection_first" )
-
-        region = self.view.sel()[0]
-        self.assertEqual( sublime.Region(0, 4), region )
-
-    def test_first_selection_with_6_selections_at_first_word(self):
-        self.create_test_text(0)
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "single_selection_first" )
-
-        region = self.view.sel()[0]
-        self.assertEqual( sublime.Region(0, 4), region )
-
-    def test_first_selection_with_6_selections_plus_redundant_expand_at_first_word(self):
-        self.create_test_text(0)
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "find_under_expand" )
-        self.view.window().run_command( "single_selection_first" )
-
-        region = self.view.sel()[0]
-        self.assertEqual( sublime.Region(0, 4), region )
-
-    def test_last_selection_with_1_selections_at_last_word(self):
+    def test_1_selections_at_last_word(self):
         self.create_test_text(0)
         self.view.window().run_command( "find_under_expand" )
         self.view.window().run_command( "single_selection_last" )
@@ -87,7 +16,7 @@ class ClearCursorsCaretsLastSelectionUnitTests(sys.modules["ClearCursorsCarets.t
         region = self.view.sel()[0]
         self.assertEqual( sublime.Region(0, 4), region )
 
-    def test_last_selection_with_2_selections_at_last_word(self):
+    def test_2_selections_at_last_word(self):
         self.create_test_text(0)
         self.view.window().run_command( "find_under_expand" )
         self.view.window().run_command( "find_under_expand" )
@@ -96,7 +25,7 @@ class ClearCursorsCaretsLastSelectionUnitTests(sys.modules["ClearCursorsCarets.t
         region = self.view.sel()[0]
         self.assertEqual( sublime.Region(5, 9), region )
 
-    def test_last_selection_with_6_selections_at_last_word(self):
+    def test_6_selections_at_last_word(self):
         self.create_test_text(0)
         self.view.window().run_command( "find_under_expand" )
         self.view.window().run_command( "find_under_expand" )
@@ -109,7 +38,7 @@ class ClearCursorsCaretsLastSelectionUnitTests(sys.modules["ClearCursorsCarets.t
         region = self.view.sel()[0]
         self.assertEqual( sublime.Region(26, 30), region )
 
-    def test_last_selection_with_6_selections_plus_redundant_expand_at_last_word(self):
+    def test_6_selections_plus_redundant_expand_at_last_word(self):
         self.create_test_text(0)
         self.view.window().run_command( "find_under_expand" )
         self.view.window().run_command( "find_under_expand" )
