bb9c6390e2b361f46e93339dd4938122ad9fc446
evandrocoan <evandrocoan@hotmail.com>
2018-01-06 14:59:29 -0200

    Renamed CURRENT_DIRECTORY/PACKAGE_NAME to ROOT_DIRECTORY/PACKAGE

--- tests/clear_cursors_carets_first_selection_unit_tests.py
+++ tests/clear_cursors_carets_first_selection_unit_tests.py
@@ -7,8 +7,8 @@ import os
 import sys
 import importlib
 
-CURRENT_DIRECTORY    = os.path.dirname( os.path.dirname( os.path.realpath( __file__ ) ) )
-CURRENT_PACKAGE_NAME = os.path.basename( CURRENT_DIRECTORY ).rsplit('.', 1)[0]
+PACKAGE_ROOT_DIRECTORY = os.path.dirname( os.path.dirname( os.path.realpath( __file__ ) ) )
+CURRENT_PACKAGE_NAME = os.path.basename( PACKAGE_ROOT_DIRECTORY ).rsplit('.', 1)[0]
 
 utilities = importlib.import_module( CURRENT_PACKAGE_NAME + ".tests.utilities" )
 
